fix(aims): fixed the FHI-aims write overflow bug that caused at when the atom's index exceeded 999 in large system
aims output the atom_index (at.) to basis-indices.out file using write(I3,...), which means a maximum of three-digit integers, beyond which it becomes “***”.

deepx_dock/convert/fhi_aims/aims_to_deeph.py
@@ -150,6 +150,64 @@ def _parse_struct(aims_dir_path: Path):
 
     return is_periodic, lat, site_positions, element, species, sort_idxs
 
+def _check_and_fix_basis_idx(aims_dir_path: Path):
+    '''
+    basis-indices.out file:
+        
+        >>>   (blank)
+        >>>   fn.   type   at.   n   l   m
+        >>>    1  atomic    1    1   0   0
+        >>>   ...
+
+    we will check whether the at.(atom_index) is int, not *** (because of the aims output setting that write at. with I3, >999 atom will output ***.)
+    
+    and the at. match the order of n.
+    '''
+    basis_path = Path(aims_dir_path) / AIMS_BASIS_FILENAME
+
+    with open(basis_path, 'r') as bs_idx_file:
+        basis_info = bs_idx_file.readlines()
+    
+    this_line_info = []
+    prev_line_info = []
+    this_line_atom_index = 1
+
+    for idx_line, line_info in enumerate(basis_info):
+        if line_info.strip() == '':
+            continue
+        this_line_info = line_info.split()
+        if this_line_info[0].startswith('fn'):
+            continue
+        if len(this_line_info) < 6:
+            raise ValueError(f"Invalid basis line: {line_info}")
+        
+        if len(prev_line_info) == 0:
+            prev_line_info = this_line_info.copy()
+            continue
+        
+        try:
+            this_line_atom_index = int(this_line_info[2])  # if atom_index is int
+        except ValueError: # while ***
+            prev_n = int(prev_line_info[3])
+            this_n = int(this_line_info[3])
+            assert this_n > 0, f"Invalid basis line: {this_line_info}"
+            if this_n == 1:
+                assert (int(this_line_info[4]) == 0 and int(this_line_info[5]) == 0), f"Invalid basis line: {this_line_info}" # n=1, l=0, m=0
+                if this_n < prev_n:
+                    this_line_atom_index += 1
+                elif prev_n == 1: # H
+                    if (prev_line_info[1] == this_line_info[1] and prev_line_info[3] == this_line_info[3]
+                        and prev_line_info[4] == this_line_info[4] and prev_line_info[5] == this_line_info[5]):
+                        this_line_atom_index +=1
+            this_line_info[2] = str(this_line_atom_index)
+            skips = ["   ", " ", "      ", "   ", "   ", "   "]
+            # basis_info = skips[0]+info[0]+skips[1]+info[1]+...
+            basis_info[idx_line] = "".join([skips[i] + this_line_info[i] for i in range(len(this_line_info))]) + "\n"
+        prev_line_info = this_line_info.copy()
+
+        with open(basis_path, 'w') as bs_idx_file:
+            bs_idx_file.writelines(basis_info)
+
 def _parse_basis(aims_dir_path: Path, atomic_num: int, species: list[str], sort_idxs: np.ndarray):
     basis_path = Path(aims_dir_path) / AIMS_BASIS_FILENAME
 
@@ -516,6 +574,7 @@ class FHIAimsReader:
         self.element, self.species, sort_idxs = _parse_struct(self.aims_path)
         assert self.is_periodic, "Only periodic system is supported!"
         # ------------ basis info from basis-indices.out ------------
+        _check_and_fix_basis_idx(self.aims_path)
         self.phase_factor, self.orbit_quantity_list, self.atom_elem_dict, \
         self.elem_orb_map, self.basis_trans_index, N_atom, N_orb, self.sub_idx = _parse_basis(
             self.aims_path, len(self.element), self.species, sort_idxs
@@ -676,6 +735,6 @@ class FHIAimsReader:
 
     # TODO: parallel HDF5 support case aims_save_type='hdf5'
     # TODO: density matrix, real-space grid V, etc
-    # TODO: only dump S if we can calc S separately?
+    # DONE: only dump S by setting sc_iter_limit = 0
     # TODO: support non-collinear spin and SOC cases
     